a24e77b63e80d4b55e70325d1ba7df8cb0148fda
Kevin <ksotillo@gmail.com>
2019-01-26 00:44:07 -0400

    Login and register views added

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,9 @@ const express = require("express");
 const router = express.Router();
 // const controlardorPrueba = require("../controllers/pruebaController");
 
-router.get("/", (req, res) => {
-  res.redirect('/index');
-});
+// router.get("/", (req, res) => {
+//   res.redirect('/');
+// });
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
@@ -14,6 +14,16 @@ router.get('/', (req, res, next) => {
 /* GET Flights Administration page. */
 router.get('/adminflights', (req, res, next) => {
   res.render('adminflights', { title: 'Guacamaya Airlines' });
+});
+
+/* GET Login page. */
+router.get('/login', (req, res, next) => {
+  res.render('login', { title: 'Guacamaya Airlines' });
+});
+
+/* GET Register page. */
+router.get('/register', (req, res, next) => {
+  res.render('register', { title: 'Guacamaya Airlines' });
 });
 
 // router.get('/a', (req, res) => {
