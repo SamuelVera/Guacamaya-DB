deff82a321619aac634f61c533c835c7e7448df2
Kevin <ksotillo@gmail.com>
2019-01-27 10:50:35 -0400

    New style added, now add button is working

--- public/js/test.js
+++ public/js/test.js
@@ -4,12 +4,13 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.fixed-action-btn');
     var instances = M.FloatingActionButton.init(elems, {direction: "left"})});
 
-    $(".responsive-table").click(function(){
+    $(".responsive-table tr").click(function(){
         $(this).addClass('selected').siblings().removeClass('selected');
         var value=$(this).find('td:first').html();
         alert(value);
      });
      
-     $('.ok').on('click', function(e){
-         alert($(".responsive-table.selected td:first").html());
+     $('.action').on('click', function(e){
+         alert($(".responsive-table tr.selected td:first").html());
      });
+
